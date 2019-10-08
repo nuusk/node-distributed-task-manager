f35f5e6955f23b07616930bbca651938357813d2
Poe <piotr.a.ptak@icloud.com>
2019-10-08 18:20:43 +0200

    Configure dotenv

--- client.js
+++ client.js
@@ -1,14 +1,20 @@
+require('dotenv').config();
+
 const zmq = require('zeromq');
 const { seconds } = require('./helpers/time');
 
+const { PORT, HOST, DEBUG_ENABLED } = process.env;
+
+const port = PORT || 4044;
+const host = HOST || '127.0.0.1';
+
 const sock = zmq.socket('req');
 
-sock.connect('tcp://127.0.0.1:3000');
-console.log('Bound to port 3000');
+sock.connect(`tcp://${host}:${port}`);
 
 setInterval(() => {
-  console.log('sending a command');
   const command = 'ls';
+  console.log(`sending a command ${command}`);
   sock.send(['task', command]);
 }, seconds(1));
 
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+require('dotenv').config();
+
 const zmq = require('zeromq');
 const { exec } = require('child_process');
 
 const sock = zmq.socket('rep');
 
-sock.bindSync('tcp://127.0.0.1:3000');
+const { PORT, HOST, DEBUG_ENABLED } = process.env;
+
+const port = PORT || 4044;
+const host = HOST || '127.0.0.1';
+
+console.log(`tcp://${host}:${port}`);
+sock.bindSync(`tcp://${host}:${port}`);
 
 sock.on('message', (topicBuffer, message) => {
   console.log('He wants me to execute a command');
